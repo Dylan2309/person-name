030908c794c05f2cc5a88e7b484eeb56d126a600
Dylan Bagnall <dylan.bagnall23@gmail.com>
2026-07-31 15:58:46 +0800

    person.ts file made

---
+++ src/types/person.ts
@@ -0,0 +1,36 @@
+// made this file to keep the data model seperate from UI
+// https://www.typescriptlang.org/docs/handbook/2/objects.html used as refernce
+
+// starting with just 1st and last name for child
+export interface Child {
+    firstName: string;
+    lastName: string;
+}
+
+// adress nested inside "person"
+export interface Address {
+    street: string;
+    city: string;
+    country: string;
+}
+// again starting with 1st and last name for person, and address and children as nested objects from above
+export interface Person {
+    firstName: string;
+    lastName: string;
+    address: Address;
+    children: Child[];
+}
+
+// https://vuejs.org/guide/essentials/reactivity-fundamentals.html#declaring-reactive-state used as reference for creating empty person object
+export function createEmptyPerson(): Person {
+    return {
+        firstName: '',
+        lastName: '',
+        address: {
+            street: '',
+            city: '',
+            country: ''
+        },
+        children: []
+    };
+}
